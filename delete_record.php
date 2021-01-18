be5d0f7e564e3a77d792bd99669d6e698b6ecf9d
joeyipdellemc <joe.yip@dell.com>
2021-01-18 21:15:51 +0800

    DB delete function done

--- delete_record.php
+++ delete_record.php
@@ -1,27 +1,17 @@
+
 <?php
-
 require_once 'connection.php';
-
 $get_id  = $_GET['get_id'];
-echo $get_id;
 
-foreach ($getid as $id){
-    echo "$id";
-}
+$sql = "DELETE FROM table_record WHERE id=$get_id"; 
 
-$colors = array("red", "green", "blue", "yellow");
+if ($conn->query($sql) === TRUE) {
+   
+    echo "Deleted Record Successfully";
+    
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 
-foreach ($colors as $value) {
-  echo "$value <br>";
-}
-
-    /*
-    $sql = "DELETE FROM table_record WHERE id=$get_id";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-*/
 $conn->close();
 ?>
--- script/delete_script.js
+++ script/delete_script.js
@@ -1,38 +1,29 @@
-$(document).ready(function(){
-
-  $('#delete').click(function(){
-
-    var post_arr = [];
-
-    // Get checked checkboxes
-    $('#tableRecord input[type=checkbox]').each(function() {
-      if (jQuery(this).is(":checked")) {
-        var id = this.id;
-        var splitid = id.split('_');
-        var postid = splitid[1];
-
-        post_arr.push(postid);
-        
-      }
-    });
-
-    if(post_arr.length > 0){
-
-        var isDelete = confirm("Do you really want to delete records?");
-        if (isDelete == true) {
-           // AJAX Request
-           $.ajax({
-              url: 'delete_record.php',
-              type: 'POST',
-              data: { post_id: post_arr},
-              success: function(response){
-                 $.each(post_arr, function( i,l ){
-                     $("#tr_"+l).remove();
-                 });
-              }
-           });
-        } 
-    } 
-  });
- 
+$(document).ready(function($) {
+  $('#result').load('test.php', function() {
+   alert('Load was performed.');
+ });
 });
+
+
+/*
+  echo "document loaded";
+  $("button").click(function(){
+		//get the botton ID
+		var $id = this.id;
+    var isDelete = confirm("Do you really want to delete record?");
+      if (isDelete == true) {
+        // AJAX Request
+        $.ajax({
+          url: 'delete_record.php',
+          type: 'GET',
+          data: {get_id: id},
+          success: function(response){
+            $.each(id, function( i,l ){
+            $("#tr_"+l).remove();
+            });
+          }
+        });
+      }; 
+  });
+
+  */
