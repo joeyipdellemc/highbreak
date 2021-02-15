ffb35e45fbed361e7bdef1ce5c722fd7efe45ba3
joeyipdellemc <joe.yip@dell.com>
2021-02-15 15:05:08 +0800

    report

--- report.php
+++ report.php
@@ -85,7 +85,7 @@
 
 
 
-<script type = "text/javascript" src = "https://www.gstatic.com/charts/loader.js"></script>
+<script type = "text/javascript" src = "./charts/loader.js"></script>
 <script type = "text/javascript">
 google.charts.load('current', {packages: ['corechart']});     
 
@@ -137,7 +137,77 @@ google.charts.load('current', {packages: ['corechart']});
 
   google.charts.setOnLoadCallback(drawChart);
   </script>
+<?php
+  require_once 'connection.php';
+  $sql = "SELECT * FROM table_record ORDER BY start_time DESC";
+  $result = $conn->query($sql);
 
+  echo "<table class='table table-hover' id='tableRecord'>
+    <thead class='thead-dark'>
+      <tr>
+        <th scope='col'>ID</th>
+        <th scope='col'>Table Number</th>
+        <th scope='col'>Start Time</th>
+        <th scope='col'>Stop Time</th>
+        <th scope='col'>Chrage</th>
+        <th scope='col'>Delete</th>
+      </tr>
+    </thead>
+    <tbody>";
+  if ($result->num_rows > 0) {
+    // output data of each row 
+    while($row = $result->fetch_assoc()){
+      $id = $row["id"];
+      echo "<tr id = 'tr_$id'>";
+        echo "<th scope='row'>";
+          echo $row["id"];
+        echo "</th>";
+        echo "<th scope='row'>";
+          echo $row["table_num"];
+        echo "</th>";
+        echo "<td>";
+          echo $row["start_time"];
+        echo "</td>";
+        echo "<td>";
+          echo $row["stop_time"];
+        echo "</td>";
+        echo "<td>";
+          echo $row["charge"];
+        echo "</td>";
+
+        //echo "<td><input type='checkbox' id='del_$id'> </td>";
+        echo "<td><button type='button' class='btn btn-primary btn-sm' id='$id'>Delete</button></td>";
+      echo "</tr>";
+    } 
+  }
+  else {echo "No Data";}
+
+  echo "</tbody>
+        </table>";
+  $conn->close();
+  ?>
+  
+  </div>
+</div>
+
+</div>
+
+  <script>
+    $("button").click(function(){
+      console.log(this.id);
+      var $id = this.id;
+      var isDelete = confirm("Do you really want to delete record ID " + $id + " ?");
+      if (isDelete == true) {
+        // AJAX Request
+        $.ajax({
+          url: 'delete_record.php',
+          type: 'GET',
+          data: {get_id: $id},
+          success: function(response){$("#tr_"+$id).remove();}
+        });
+      }
+    });
+  </script>
 
 </body>
 </html>
